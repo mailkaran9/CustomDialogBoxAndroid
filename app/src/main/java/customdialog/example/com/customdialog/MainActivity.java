ac0301394a362074110bff2f64640d009d58cfc9
KARAN GULATI <mailkaran9@gmail.com>
2018-11-10 20:20:46 -0500

    Using the library in the demo app

--- app/src/main/java/customdialog/example/com/customdialog/MainActivity.java
+++ app/src/main/java/customdialog/example/com/customdialog/MainActivity.java
@@ -1,7 +1,10 @@
 package customdialog.example.com.customdialog;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import customdialog.example.com.customdialogboxandroid.CustomFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+
+        String permissionReason = "App would like to request for access to your contact list to enable you to find friends on the app";
+        CustomFragment cf = CustomFragment.newInstance(permissionReason,"","","","","");
+        cf.show(fm, "fragment_edit_name");
+
+
     }
 }
